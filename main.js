c0348b1f364425e5467565facb73b7ec7fafe1be
BarreraB <33660732+BarreraB@users.noreply.github.com>
2018-03-04 12:44:11 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -69,28 +69,117 @@ $(function() {
 	});
 });
 
+
+
+/*******************************************
+********************************************
+************ ANIMATE FOOTER LOGO ***********
+********************************************
+*******************************************/
+    var pix = $('.claimpix-logo');
+    function animateLogo() {
+    	var status = pix.hasClass('active');
+    	if(status) {
+    		pix.removeClass('active');
+    	} else {
+    		pix.addClass('active');
+    	}
+
+    }
+    $('.claimpix-logo').click(animateLogo);
+  
+
+    var navItem = $('itemClass');
+    function animateLinks(item) {
+    	var status = navItem.hasClass('active');
+    	if(status) {
+    		item.removeClass('active');
+    	} else {
+    		item.addClass('active');
+    	}
+   }
+
+   // $('.footer-item').mouseenter(animateLinks);
+    //$('.footer-item').mouseleave(animateLinks);
+
+    function footerItemEvent(itemClass) {
+    	var navItem = $(itemClass);
+    	$(itemClass).mouseenter(function(){
+    		animateLinks(navItem);
+    	});
+
+    	$(itemClass).mouseleave(function(){
+    		animateLinks(navItem);
+    	});
+
+    }
+
+    footerItemEvent('.footer-item');   
+    footerItemEvent('.footer-item-1');
+    footerItemEvent('.footer-item-2');
+
+
+
+
 /***************************
 ****************************
 **** FORM VALIDATION *******
 ****************************
 ***************************/
 
-var form = document.querySelector('form');
-var email = document.querySelector('#mail');
-var error = document.querySelector('.error');
+//var form = document.querySelector('form');
+//var email = document.querySelector('#mail');
+//var error = document.querySelector('.error');
 
-email.addEventListener('input', function(event) {
-	if(email.validity.valid) {
-		error.innerHTML = '';
-		error.className = 'error';
-	}
-});
-form.addEventListener('submit', function(event) {
+//email.addEventListener('input', function(event) {
+//	if(email.validity.valid) {
+//		error.innerHTML = '';
+//		error.className = 'error';
+//	}
+//});
+//form.addEventListener('submit', function(event) {
 
-	event.preventDefault();
-	if(!email.validity.valid) {
-		error.innerHTML = 'Insert valid email address';
-		error.className = 'error active';
-	}
+//	event.preventDefault();
+//	if(!email.validity.valid) {
+//		error.innerHTML = 'Insert valid email address';
+//		error.className = 'error active';
+//	} else {
+  //    // submit the form
+      // AJAX
+    //  $.ajax({
+      //  type: 'POST',
+        //url: '/',
+       // data: { email: email.value },
+     // }).done(function(response){
+       // console.log("Successfully sent the form!");
+     // });
 
-});
+//}
+//});
+
+
+ //var circ = document.querySelector('.claimpix-logo');
+    //circ.addEventListener('click', function(){
+      //var status = circ.classList.contains('active');
+
+      //if(status) {
+       // circ.classList.remove('active');
+      //} else {
+       // circ.classList.add('active');
+      //}
+    //});
+
+
+
+
+    //var navFooter = $('.footer-item-1');
+    //function animateSecLink() {
+    	//var status = navFooter.hasClass('active');
+    	//if(status) {
+    		//navFooter.removeClass('active');
+    	//} else {
+    		//navFooter.addClass('active');
+    	//}
+    //}
+
+   // $('.footer-item-1').mouseenter(animateSecLink);
